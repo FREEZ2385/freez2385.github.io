4640254eddfdfcbc03255e855ca1b3ec4ef97ef0
Freez <schlee9703@naver.com>
2023-01-16 09:28:37 +0900

    [Post]2023년 1월 16일 월요일 09시 28분 37초 KST

---
+++ tools/_site/init.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Initial the Categories/Tags pages and Lastmod for posts.
+# v2.0
+# https://github.com/cotes2020/jekyll-theme-chirpy
+# © 2019 Cotes Chung
+# Published under MIT License
+
+set -eu
+
+CATEGORIES=false
+TAGS=false
+LASTMOD=false
+
+WORK_DIR=$(dirname $(dirname $(realpath "$0")))
+
+check_status() {
+  if [[ ! -z $(git status -s) ]]; then
+    echo "Warning: Commit the changes of the repository first."
+    git status -s
+    exit 1
+  fi
+}
+
+
+update_files() {
+  python _scripts/py/init_all.py
+  find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+}
+
+
+commit() {
+  msg="[Update]"
+
+  if [[ ! -z $(git status categories -s) ]]; then
+    git add categories/
+    msg+="Categories"
+    CATEGORIES=true
+  fi
+
+  if [[ ! -z $(git status tags -s) ]]; then
+    git add tags/
+    if [[ $CATEGORIES = true ]]; then
+      msg+=" /"
+    fi
+    msg+=" Tags"
+    TAGS=true
+  fi
+
+  if [[ ! -z $(git status _posts -s) ]]; then
+    git add _posts/
+    if [[ $CATEGORIES = true || $TAGS = true ]]; then
+      msg+=" /"
+    fi
+    msg+=" Lastmod"
+    LASTMOD=true
+  fi
+
+  if [[ $CATEGORIES = true || $TAGS = true || $LASTMOD = true ]]; then
+    git commit -m "[Update] $msg"
+  else
+    msg="Nothing changed."
+  fi
+
+  echo $msg
+}
+
+
+main() {
+
+  cd $WORK_DIR
+
+  check_status
+
+  update_files
+
+  commit
+}
+
+main
---
+++ tools/_site/page-build.sh
@@ -0,0 +1,7 @@
+git checkout master
+rm -rf _site/
+bundle exec jekyll build
+git add --all
+git commit -m "[Post]`date`"
+git push origin master
+git subtree push --prefix=_site/ origin gh-pages
