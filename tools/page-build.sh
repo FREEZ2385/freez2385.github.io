c254509d44d2387545dcde433c0d15ad298752f5
Freez <schlee9703@naver.com>
2025-05-18 16:18:51 +0900

    [update] POST

--- tools/page-build.sh
+++ tools/page-build.sh
@@ -4,6 +4,6 @@ git checkout master
 rm -rf _site/
 bundle exec jekyll build
 git add --all
-git commit -m "`date`"
+git commit -m "[Post]`date`"
 git push origin master
 git subtree push --prefix=_site/ origin gh-pages
