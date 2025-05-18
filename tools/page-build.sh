f9f76686a21abcdf2f204c644558d20a1eae9838
Freez <schlee9703@naver.com>
2025-05-18 16:00:24 +0900

    [modify] add mise.tool

--- tools/page-build.sh
+++ tools/page-build.sh
@@ -1,9 +1,7 @@
-set -e
+#!/bin/bash:
 
 rm -rf _site/
 git checkout master
-docker build -t freezblog:0.1 ./
-docker run -v $(pwd)/_site:/mydir/_site freezblog:0.1
 git add --all
 git commit -m "[Post]`date`"
 git push origin master
